4142874daab726b4fc7bfdb7e9f2fd6913ed12fb
jerry <trogvar22@gmail.com>
2016-03-18 19:14:58 +0300

    add sms ISZ16-91

--- module/smsSender.js
+++ module/smsSender.js
@@ -2,16 +2,18 @@ var fetch = require('whatwg-fetch');
 var config = require('../config');
 var url = config.sms.url;
 
+module.exports = {
+    //http://sms.ru/sms/send?api_id=30DE7FDE-B7EE-B1A2-EC92-06162C32C280&to=79381566518&text=hello+world
+    sendSmsAsync: function sendSmsAsync(sms) {
+        var text = sms.text.replace(' ', '+');
 
-//http://sms.ru/sms/send?api_id=30DE7FDE-B7EE-B1A2-EC92-06162C32C280&to=79381566518&text=hello+world
-var sendSmsAsync = function sendSmsAsync(sms)
-{
-    var text = sms.text.split(' ').join('+');
-    fetch(url + '&to=' + sms.to + '&text=' + text + '&test=1', {
-        method: 'get'
-    }).then(function (response) {
-        return response;
-    });
-}
-
-module.exports = sendSmsAsync;
+        fetch(url + '&to=' + sms.to + '&text=' + text + '&test=1', {
+            'method': 'get'
+        }).then(function (response) {
+            console.log('/////////////////start/////////////////');
+            console.log(response);
+            console.log('////////////////end//////////////////');
+            return response;
+        });
+    }
+};
